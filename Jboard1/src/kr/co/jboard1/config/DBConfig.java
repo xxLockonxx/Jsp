c58dd11b973251ceed9e99b09e3e9233a3bae5bd
xxLockonxx <ptsgrd@naver.com>
2020-06-16 15:30:05 +0900

    ss

--- Jboard1/src/kr/co/jboard1/config/DBConfig.java
+++ Jboard1/src/kr/co/jboard1/config/DBConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConfig {
 
 	// DB정보 - 배포용
-	//public static final String HOST = "jdbc:mysql://13.125.215.118:3306/lsj";
+	//public static final String HOST = "jdbc:mysql://54.180.141.24/lsj";
 	//public static final String USER = "root";
 	//public static final String PASS = "tjalvlrhs";
 	
